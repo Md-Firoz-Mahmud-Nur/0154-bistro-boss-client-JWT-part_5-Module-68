3f5843106dcbc420fd741d47f4ac71facb78d585
firoz.nur <firoznur5@gmail.com>
2024-06-04 23:25:22 +0600

    update

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -27,7 +27,7 @@ const useAxiosSecure = () => {
     }, async (error) => {
         const status = error.response.status;
         // console.log('status error in the interceptor', status);
-        // for 401 or 403 logout the user and move the user to the login
+        // for 401 or 403 logout the user and move the user to the login page
         if (status === 401 || status === 403) {
             await logOut();
             navigate('/login');
